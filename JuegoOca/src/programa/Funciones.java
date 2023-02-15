0553dc81c2fed98ad9486b4fc468a9e468c26a35
fmigg <fmigg@Fernando>
2023-02-15 20:04:10 +0100

    Completado

--- JuegoOca/src/programa/Funciones.java
+++ JuegoOca/src/programa/Funciones.java
@@ -1,86 +1,112 @@
 package programa;
 
-import org.w3c.dom.bootstrap.DOMImplementationRegistry;
-
 public class Funciones {
-	static final int TAMCIRCUITO = 11;
-	static int fichaJ1, fichaJ2;
-	static int minDado, maxDado;
+    // ^ Definimos la variable TAMCIRCUITO como final y global
+    static final int TAMCIRCUITO = 11;
 
-	static int tiraDados() {
-		int resultado;
-		resultado = (int) Math.random() * (maxDado - minDado) + minDado;
-		return resultado;
-	}
+    // ^ Definimos las variables fichaJ1 y fichaJ2 como variables globales no estaticas, estas variables almacenarán la posición de los Jugadores
+    static int fichaJ1, fichaJ2;
 
-	static void pintaPista() {
-		for (int i = 1; i <= TAMCIRCUITO; i++) {
-			System.out.print("\t" + i);
-		}
+    // ^ Definimos las variables min y max para el dado, estas variables son globales estaticas y sirven para indicarle al programa el min y maximo de un dado
+    static int minDado;
+    static int maxDado;
 
-		System.out.println();
+    // ? El método tiraDados devolverá un número pseudoaleatorio entre el min y el max del dado
+    static int tiraDados(){
+        // ! Generamos un número double
+        double resultado = Math.random() * (maxDado - minDado) + minDado;
 
-		for (int pos = 0; pos <= fichaJ1; pos++) {
-			if (pos == fichaJ1) {
-				System.out.print("J1");
-			} else {
-				System.out.print(" ");
-			}
-			System.out.print("\t");
-		}
+        // ! Sumamos 0.5 para redondearlo
+        resultado += 0.5d;
 
-		System.out.println();
+        // ! Devolvemos el número redondeado a un entero
+        return (int) resultado;
+    }
 
-		for (int pos = 0; pos <= fichaJ2; pos++) {
-			if (pos == fichaJ2) {
-				System.out.print("J2");
-			} else {
-				System.out.print(" ");
-			}
-			System.out.print("\t");
-		}
-		
-		System.out.println();
-	}
+    // ? El método pintaPista estático pintara un tablero según el tama;o de la pista y la posición de los Jugadores
+    static void pintaPista(){
+        // ! Primero pintamos todo el tablero
+        for (int pos = 0; pos <= TAMCIRCUITO; pos++){
+            if (pos == 0){
+                System.out.println("\t ");
+            } else {
+                System.out.print("\t" + pos);
+            }
+        }
 
-	static boolean sumaDeNumerosEsPrimo(int tirada1, int tirada2) {
-		int resultado = tirada1 + tirada2;
-		boolean estado = true;
-		for (int i = 2; i < resultado; i++) {
-			if (resultado % i == 0||resultado<=1) {
-				estado = false;
-			}
+        System.out.println(); // ? Cambiamos de Línea
+        
+        // ! Despues pintaremos la posición del Jugador 1
+        for (int pos = 0; pos <= fichaJ1; pos++){
+            if (pos == fichaJ1){
+                System.out.print("J1");
+            } else{
+                System.out.print(" ");
+            } System.out.print("\t");
+        }
 
-		}
+        System.out.println(); // ? Cambiamos de Línea
+        
+        // ! Por último pintaremos la posición del Jugador 2
+        for (int pos = 0; pos <= fichaJ2; pos++){
+            if (pos == fichaJ2){
+                System.out.print("J2");
+            } else{
+                System.out.print(" ");
+            } System.out.print("\t");
+        }
+    }
 
-		return estado;
-	}
+    // ? Esta función estatica devolverá true si la suma de la tirada es prima y false si la tirada np es prima
+    static boolean sumaDeNumerosEsPrimo(int tirada1, int tirada2){
+        // ^ Definimos la variable resultado que sera igual a la suma de las dos tiradas
+        int resultado = tirada1 + tirada2;
 
-	void imprimeComoVaLaCarrera(String nomJ1, String nomJ2) {
-		if (fichaJ1 > fichaJ2) {
-			System.out.println("Va ganando " + nomJ1);
-		}
+        // ^ Definimos la variable state la cual cambiará según si es primo o no
+        boolean state = true;
 
-		else if (fichaJ2 > fichaJ1) {
-			System.out.println("Va ganando " + nomJ2);
-		}
+        // ! 1 o menor nunca será primo, en case contrario lo comprobaremos
+        if(resultado <= 1){
+            state = false;
+        } else{
+            for (int i = 2; i < resultado; i++){
+                if (resultado % i == 0){ // ! En case de que alguno de los modulos de 0 no será primo
+                    state = false;
+                }
+            }
+        }
 
-		else {
-			System.out.println("Van empatados");
-		}
-	}
+        // ! Si pasa todo lo anterior será primo, por lo que devolveremos true
+        return state;
+    }
 
-	String esGanador(String nomJ1, String nomJ2) {
-		String resultado = "";
-		if (fichaJ1 >= TAMCIRCUITO) {
-			resultado = nomJ1;
-		}
+    // ? Este método imprimirá el nombre de la persona que va ganando
+    void imprimeComoVaLaCarrera(String nomJ1, String nomJ2){
+        // ! Mediante una estructura de if-else combroparemos quien va en cabeza
+        if (fichaJ1 > fichaJ2){ // * Si J1 va ganando
+            System.out.println("'" + nomJ1 + "' va Ganando");
+        } else if (fichaJ2 > fichaJ1){ // * Si J2 va ganando
+            System.out.println("'" + nomJ2 + "' va Ganando");
+        } else { // * Si van empate
+            System.out.println("'" + nomJ1 + "' y '" + nomJ2 + "' van Empatados");
+        }
+    }
 
-		else if (fichaJ2 >= TAMCIRCUITO) {
-			resultado = nomJ2;
-		}
+    // ? El método esGanador devolverá el nombre del quien haya ganado, si esque alguién gano
+    String esGanador(String nomJ1, String nomJ2){
+        // ^ Definimos la variable ganador que ira cambiando según el ganador
+        String ganador;
 
-		return resultado;
-	}
+        // ! Según quién haya superado el tablero mandaremos un nombre o otro
+        if (fichaJ1 >= TAMCIRCUITO){ // * Si J1 a ganado
+            ganador = nomJ1;
+        } else if (fichaJ2 >= TAMCIRCUITO){ // * Si J2 a ganado
+            ganador = nomJ2;
+        } else {
+            ganador = "";
+        }
 
+        // ! Devolvemos el nombre del Ganador
+        return ganador;
+    }
 }
--- JuegoOca/src/programa/Main.java
+++ JuegoOca/src/programa/Main.java
@@ -1,88 +1,132 @@
 package programa;
 
+// ? Importamos la clase Scanner y Locale
 import java.util.Scanner;
+import java.util.Locale;
 
 public class Main {
-
 	public static void main(String[] args) {
-		Funciones metodo = new Funciones ();
-		
-		int tirada1, tirada2;
-		String nomJ1, nomJ2;
-		Scanner read = new Scanner (System.in);
-		
-		System.out.println("Introduce el número mínimo del dado: ");
-		Funciones.minDado=read.nextInt();
-		System.out.println("Introduce el número máximo del dado: ");
-		Funciones.maxDado=read.nextInt();
-			
-		
-		do {
-			System.out.println("Introduce el nombre del primer jugador: ");
-			nomJ1=read.next();
-			System.out.println("Introduce el nombre del segundo jugador: ");
-			nomJ2=read.next();
-		} while (nomJ1.equals(nomJ2));
-		
-		do {
-			System.out.println("Pulsa culauier tecla:");
-			read.next();
-			
-			tirada1=Funciones.tiraDados();
-			System.out.println("Primera tirada jugador 1: " + Funciones.tiraDados());
-			
-			tirada2=Funciones.tiraDados();
-			System.out.println("Segundo tirada jugador 1: " + Funciones.tiraDados());
-			
-			
-			if (Funciones.sumaDeNumerosEsPrimo(tirada1, tirada2)) {
-				Funciones.fichaJ1+=tirada1+tirada2;
-				System.out.println("La jugada es válida");
-			}
-			
-			else {
-				System.out.println("La tirada no es válida");
-			}
-			
-			Funciones.pintaPista();
-			
-			if (Funciones.fichaJ1>=Funciones.TAMCIRCUITO) {
-				break;
-			}
-		
-			metodo.imprimeComoVaLaCarrera(nomJ1, nomJ2);			
-			
-			System.out.println("Pulsa culauier tecla:");
-			read.next();
-			
-			tirada1=Funciones.tiraDados();
-			System.out.println("Primera tirada jugador 2: " + Funciones.tiraDados());
-			
-			tirada2=Funciones.tiraDados();
-			System.out.println("Segundo tirada jugador 2: " + Funciones.tiraDados());
-			
-			
-			if (Funciones.sumaDeNumerosEsPrimo(tirada1, tirada2)) {
-				Funciones.fichaJ2+=tirada1+tirada2;
-				System.out.println("La jugada es válida");
-			}
-			
-			else {
-				System.out.println("La tirada no es válida");
-			}
-			
-			Funciones.pintaPista();
-			
-			if (Funciones.fichaJ1>=Funciones.TAMCIRCUITO) {
-				break;
-			}
-			
-			metodo.imprimeComoVaLaCarrera(nomJ1, nomJ2);
-			
-			
-		}while (metodo.esGanador(nomJ1, nomJ2).equals(""));
-		
-		System.out.println("Ha ganado " + metodo.esGanador(nomJ1, nomJ2));
-	}
+		// ^ Definimos una instancia de la clase Oca
+		Funciones game = new Funciones();
 
+		// ^ Definimos una instancia de la clase Scanner y cambiamos su formato a US
+		Scanner read = new Scanner(System.in).useLocale(Locale.US);
+
+		// ^ Definimos las variables tirada1 y tirada2 que almacenaran el resultado de
+		// las dos tiradas
+		int tirada1, tirada2;
+
+		// ^ Definimos las variables nomJ1 y nomJ2 para almacenar el nombre de los
+		// jugadores
+		String nomJ1, nomJ2;
+
+		// ! Presentamos a los Usuarios el Juego
+		System.out.println("Bienvenidos a el Juego 'La Oca de los numeros Primos!!!'");
+		System.out.println("Para jugar debereis introducir vuestros nombres y el valor mínimo y máximo del dado.");
+		System.out.println("Vuestros nombres no podrán ser iguales.");
+		System.out.println("Dicho esto empezemos.");
+
+		// ? Salto de Línea Triple
+		System.out.println("\n-------------------------\n");
+
+		// ! Pedimos el valor mínimo y máximo del dado
+		System.out.print("Introducid el valor mínimo del dado > ");
+		Funciones.minDado = read.nextInt(); // * Leemos el valor mínimo del dado
+		System.out.print("Introducid el valor máximo del dado > ");
+		Funciones.maxDado = read.nextInt(); // * Leemos el valor máximo del dado
+
+		// ? Salto de Línea Triple
+		System.out.println("\n---------------------\n");
+
+		read.nextLine(); // ? Limpiamos el Búfer del Scanner tras leer un número para poder leer un
+							// String
+
+		// ! Pedimos los nombres a los Usuarios
+		System.out.print("Como te llamas, Jugador 1? > ");
+		nomJ1 = read.nextLine(); // * Leemos el nombre de J1
+		do { // ! Pediremos el nombre del Jugador 2 hasta que sea diferente al del jugador 1
+			System.out.print("Como te llamas, Jugador 2? > ");
+			nomJ2 = read.nextLine(); // * Leemos el nombre de J2
+			if (nomJ2.equals(nomJ1)) {
+				System.out.println("Lo siento Jugador 2, tu nombre no puede ser igual que el de tu compañero\n");
+			}
+		} while (nomJ2.equals(nomJ1));
+
+		// ! Pintamos la pista por primera vez
+		Funciones.pintaPista();
+
+		do {
+			// ? Salto de Línea
+			System.out.println("\n");
+
+			// ! Pediremos que pulsen una tecla para la siguiente ronda
+			System.out.print("Pulsa 'enter' para continuar");
+			read.nextLine();
+
+			// ! Imprimiremos las tiradas de J1
+			System.out.println("\nTurno para " + nomJ1);
+			tirada1 = Funciones.tiraDados(); // * Generamos la primera tirada
+			System.out.println("En la primera tirada ha sacado un " + tirada1);
+			tirada2 = Funciones.tiraDados(); // * Generamos la segunda tirada
+			System.out.println("En la segunda tirada ha sacado un " + tirada2);
+
+			// ! Si la suma de las tiradas es primo avanzará
+			if (Funciones.sumaDeNumerosEsPrimo(tirada1, tirada2)) { // * Si es Primo
+				System.out.println("TIRADA VÁLIDA");
+				Funciones.fichaJ1 += (tirada1 + tirada2);
+			} else { // * Si no es Primo
+				System.out.println("TIRADA NO VÁLIDA\nNo suma puntos ya que la el total NO ES PRIMO");
+			}
+
+			// ! Pintamos el Tablero tras la jugada
+			Funciones.pintaPista();
+
+			// ? Cambiamos de Línea
+			System.out.println("\n");
+
+			// ! Indicamos quien va ganando
+			game.imprimeComoVaLaCarrera(nomJ1, nomJ2);
+
+			// ! Si el Jugador 1 ha llegado antes que el Jugador 2 nos saldremos del Bucle
+			if (Funciones.fichaJ1 >= Funciones.TAMCIRCUITO) {
+				break;
+			}
+
+			// ! Pediremos que pulsen una tecla para la siguiente ronda
+			System.out.print("Pulsa 'enter' para continuar");
+			read.nextLine();
+
+			// ! Imprimiremos las tiradas de J2
+			System.out.println("\nTurno para " + nomJ2);
+			tirada1 = Funciones.tiraDados(); // * Generamos la primera tirada
+			System.out.println("En la primera tirada ha sacado un " + tirada1);
+			tirada2 = Funciones.tiraDados(); // * Generamos la segunda tirada
+			System.out.println("En la segunda tirada ha sacado un " + tirada2);
+
+			// ! Si la suma de las tiradas es primo avanzará
+			if (Funciones.sumaDeNumerosEsPrimo(tirada1, tirada2)) {
+				System.out.println("TIRADA VÁLIDA");
+				Funciones.fichaJ2 += (tirada1 + tirada2);
+			} else {
+				System.out.println("TIRADA NO VÁLIDA\nNo suma puntos ya que la el total NO ES PRIMO");
+			}
+
+			// ! Pintamos el Tablero tras la jugada
+			Funciones.pintaPista();
+
+			// ? Cambiamos de Línea
+			System.out.println("\n");
+
+			// ! Indicamos quien va ganando
+			game.imprimeComoVaLaCarrera(nomJ1, nomJ2);
+		} while (game.esGanador(nomJ1, nomJ2).equals(""));
+
+		// ^ Cerramos el Scanner tras su uso
+		read.close();
+
+		// ! Imprimimos el Ganador de la Partida
+		System.out.println("\n-------------------------------");
+		System.out.println("El/La ganador/a es " + game.esGanador(nomJ1, nomJ2));
+		System.out.println("-------------------------------");
+	}
 }
